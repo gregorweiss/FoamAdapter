8b70a2ca8ae48d8b67ddf1aaeb29b23856b44495
Gregor Weiss <gregor.weiss@hlrs.de>
2025-01-03 04:55:04 -0500

    verbose (remove after debugging)

--- examples/scalarAdvection/scalarAdvection.cpp
+++ examples/scalarAdvection/scalarAdvection.cpp
@@ -3,9 +3,12 @@
 
 #include "NeoFOAM/NeoFOAM.hpp"
 
+#include "NeoFOAM/setup.hpp"
+
 #include "FoamAdapter/FoamAdapter.hpp"
 #include "FoamAdapter/readers/foamDictionary.hpp"
 
+#include <chrono>
 
 #define namespaceFoam
 #include "fvCFD.H"
@@ -73,7 +76,7 @@ int main(int argc, char* argv[])
                 }
             );
         auto nfPhi0 = Foam::constructSurfaceField(exec, nfMesh, phi0);
-        auto nfPhi = Foam::constructSurfaceField(exec, nfMesh, phi);
+	fvcc::SurfaceField<NeoFOAM::scalar> nfPhi = Foam::constructSurfaceField(exec, nfMesh, phi);
 
         Foam::scalar endTime = controlDict.get<Foam::scalar>("endTime");
 
@@ -94,6 +97,11 @@ int main(int argc, char* argv[])
 
 
             std::tie(adjustTimeStep, maxCo, maxDeltaT) = timeControls(runTime);
+    auto start = std::chrono::system_clock::now();
+	    coNum = fvcc::computeCoNum(nfPhi, dt);
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    std::cout << "Outside elapsed time: " << elapsed_seconds.count() << "s\n";
             coNum = calculateCoNum(phi);
             Foam::Info << "max(phi) : " << max(phi).value() << Foam::endl;
             Foam::Info << "max(U) : " << max(U).value() << Foam::endl;
